3b1ae1966a473738a9ebbf504daaf63f16806479
Ridho Kuncoro Adji Wibowo <64425575+kuncoroadjie@users.noreply.github.com>
2024-11-01 11:28:09 +0700

    Implement create Contact API

---
+++ src/service/contact-service.ts
@@ -0,0 +1,23 @@
+import { prismaClient } from "../application/database";
+import { ContactResponse, CreateContactRequest, toContactResponse } from "../model/contact-model";
+import { ContactValidation } from "../validation/contact-validation";
+import { Validation } from "../validation/validation";
+import { User } from "@prisma/client";
+
+export class ContactService {
+  static async create(user: User, request: CreateContactRequest): Promise<ContactResponse> {
+    const createRequest = Validation.validate(ContactValidation.CREATE, request);
+
+    const record = {
+      ...createRequest,
+      ...{username: user.username}
+    };
+
+    const contact = await prismaClient.contact.create({
+      data: record
+    });
+
+    return toContactResponse(contact);
+
+  }
+}
